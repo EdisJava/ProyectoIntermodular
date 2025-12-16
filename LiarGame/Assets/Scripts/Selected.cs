0e86f94acfb7bad04a9f19b11b2b15e37acfee39
ametralletor <adaysg14@gmail.com>
2025-12-16 23:29:51 +0000

    Crosshair y texto temporal al entrar en alcanze con la puerta

--- LiarGame/Assets/Scripts/Selected.cs
+++ LiarGame/Assets/Scripts/Selected.cs
@@ -9,23 +9,32 @@ public class Selected : MonoBehaviour
 
     private InputAction interactAction;
 
+    public GameObject crosshair;
+    public GameObject interactText;
+    private bool isLookingAtDoor;
+
     void Start()
     {
         mask = LayerMask.GetMask("Raycast Detect");
         if (playerInput != null)
             interactAction = playerInput.actions["Interact"];
+        crosshair.SetActive(false);
+        interactText.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         RaycastHit hit;
+        isLookingAtDoor = false;
 
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, distancia, mask))
         {
             if(hit.collider.tag == "PuertaInteractiva")
             {
+                isLookingAtDoor = true;
                 Debug.Log("Objeto Seleccionado: " + hit.collider.name);
+
 
                 if (interactAction != null && interactAction.triggered)
                 {
@@ -34,5 +43,8 @@ public class Selected : MonoBehaviour
                 }
             }
         }
+
+        crosshair.SetActive(isLookingAtDoor);
+        interactText.SetActive(isLookingAtDoor);
     }
 }
