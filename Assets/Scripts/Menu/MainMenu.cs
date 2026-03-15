921b627d01c99b37f4ee17953cc205b036a13ca5
Edgar Javier Quesada Morales <edgarjqm122@gmail.com>
2026-03-15 20:59:21 +0000

    añadido confirmación

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -7,28 +7,62 @@ public class MainMenu : MonoBehaviour
   
     public GameObject mainMenu;
     public Button playButton;
+    public Button newGameButton;
+    public Button exitButton;
+    public GameObject newGameConfirmationText;
+    public Button confirmYesButton;
+    public Button confirmNoButton;
 
     void Awake()
     {
         if (playButton == null)
         {
-            GameObject playButtonObject = GameObject.Find("JugarButton");
-            if (playButtonObject != null)
+            playButton = FindButtonByName("JugarButton");
+        }
+
+        if (newGameButton == null)
+        {
+            newGameButton = FindButtonByName("NuevaPartidaButton");
+        }
+        
+        if (exitButton == null)
+        {
+            exitButton = FindButtonByName("SalirButton");
+        }
+
+        if (newGameConfirmationText == null)
+        {
+            TMPro.TextMeshProUGUI[] texts = FindObjectsByType<TMPro.TextMeshProUGUI>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+            foreach (TMPro.TextMeshProUGUI text in texts)
             {
-                playButton = playButtonObject.GetComponent<Button>();
+                if (text.text.Contains("SEGURO"))
+                {
+                    newGameConfirmationText = text.gameObject;
+                    break;
+                }
             }
+        }
+
+        if (confirmYesButton == null)
+        {
+            confirmYesButton = FindButtonByName("SiButton");
+        }
+
+        if (confirmNoButton == null)
+        {
+            confirmNoButton = FindButtonByName("noButton");
         }
     }
 
     void Start()
     {
-        RefreshButtons();
+        ShowMainOptions();
     }
 
     public void OpenMainMenuPanel()
     {
         mainMenu.SetActive(true);
-        RefreshButtons();
+        ShowMainOptions();
     }
 
     public void QuitGame()
@@ -50,9 +84,19 @@ public class MainMenu : MonoBehaviour
 
     public void NewGame()
     {
+        ShowNewGameConfirmation();
+    }
+
+    public void ConfirmNewGame()
+    {
         SaveSystem.DeleteSave();
-        RefreshButtons();
+        ShowMainOptions();
         SceneManager.LoadScene("SampleScene");
+    }
+
+    public void CancelNewGame()
+    {
+        ShowMainOptions();
     }
 
     void RefreshButtons()
@@ -61,5 +105,57 @@ public class MainMenu : MonoBehaviour
         {
             playButton.interactable = SaveSystem.HasSave();
         }
+    }
+
+    void ShowNewGameConfirmation()
+    {
+        SetActiveIfAssigned(playButton, false);
+        SetActiveIfAssigned(newGameButton, false);
+        SetActiveIfAssigned(exitButton, false);
+        SetActiveIfAssigned(newGameConfirmationText, true);
+        SetActiveIfAssigned(confirmYesButton, true);
+        SetActiveIfAssigned(confirmNoButton, true);
+        RefreshButtons();
+    }
+
+    void ShowMainOptions()
+    {
+        SetActiveIfAssigned(playButton, true);
+        SetActiveIfAssigned(newGameButton, true);
+        SetActiveIfAssigned(exitButton, true);
+        SetActiveIfAssigned(newGameConfirmationText, false);
+        SetActiveIfAssigned(confirmYesButton, false);
+        SetActiveIfAssigned(confirmNoButton, false);
+        RefreshButtons();
+    }
+
+    void SetActiveIfAssigned(Component component, bool active)
+    {
+        if (component != null)
+        {
+            component.gameObject.SetActive(active);
+        }
+    }
+
+    void SetActiveIfAssigned(GameObject target, bool active)
+    {
+        if (target != null)
+        {
+            target.SetActive(active);
+        }
+    }
+
+    Button FindButtonByName(string buttonName)
+    {
+        Button[] buttons = FindObjectsByType<Button>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+        foreach (Button button in buttons)
+        {
+            if (button.gameObject.name.Trim() == buttonName)
+            {
+                return button;
+            }
+        }
+
+        return null;
     }
 }
